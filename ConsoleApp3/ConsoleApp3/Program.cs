05fdff9a2e4990c6867a83025854c2be14c69e50
Tayfun Selim <tayfunselim@gmail.com>
2019-12-27 20:09:39 +0100

    change1

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi Tayfun");
         }
     }
 }
